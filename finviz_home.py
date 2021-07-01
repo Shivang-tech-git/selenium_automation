ffa65b474b2ff1130bea1f00af65d79aaacd0f22
Shivang-tech-git <raju.rastogi2500@gmail.com>
2021-07-01 22:17:29 +0530

    second commit

--- finviz_home.py
+++ finviz_home.py
@@ -51,46 +51,49 @@ def finviz():
 
 def save_table():
     df = pd.DataFrame()
-    signals = driver.find_elements_by_xpath('//select[@id="signalSelect"]/option')
-    signals_list = [signal.get_attribute('innerText') for signal in signals]
-    for signal in signals_list:
-        if signal != 'None (all stocks)':
-            elementFinder('//select[@id="signalSelect"]/option[text()="{}"]'.format(signal))
-            driver.find_element_by_xpath('//select[@id="signalSelect"]/option[text()="{}"]'.format(signal)).click()
-            elementFinder('//select[@id="signalSelect"]/option[text()="{}"]'.format(signal))
-            data = []
-            table = driver.find_element_by_xpath('(//div[@id="screener-content"]/descendant::table)[4]')
-            columns = [col.get_attribute('innerText') for col in table.find_elements_by_xpath('./descendant::tr[1]/td')]
-            next_page = True
-            while next_page:
-                table = driver.find_element_by_xpath('(//div[@id="screener-content"]/descendant::table)[4]')
-                body = table.find_elements_by_xpath('./descendant::tr')
-                for row in body:
-                    cells = row.find_elements_by_xpath('./td')
-                    # dont add header row
-                    if cells[1].get_attribute('innerText') != 'Ticker':
-                        one_row = []
-                        one_row = [cell.get_attribute('innerText') for cell in cells]
-                        one_row.insert(1,cells[1].find_element_by_xpath('./a').get_attribute('href'))
-                        one_row.extend(['{}'.format(signal)])
-                        data.append(one_row)
-                try:
-                    elementFinder('//a[@class="screener_arrow"]','click')
-                except:
-                    next_page = False
-            current_signal = pd.DataFrame(data)
-            df = df.append(current_signal)
-    columns.insert(1,'URL')
+    elementFinder('//select[@id="signalSelect"]')
+    columns_tags = driver.find_elements_by_xpath('(//div[@id="screener-content"]/descendant::table)[4]/descendant::tr[1]/td')
+    columns = [col.get_attribute('innerText') for col in columns_tags]
+    columns.insert(1, 'URL')
     columns.extend(['Signal'])
+    signals = pd.read_csv('signal.csv')
+    signals_list =  signals['Selected signals'].tolist()
+    signals_list = [x for x in signals_list if str(x) != 'nan']
+    for signal in signals_list:
+        elementFinder('//select[@id="signalSelect"]/option[text()="{}"]'.format(signal))
+        driver.find_element_by_xpath('//select[@id="signalSelect"]/option[text()="{}"]'.format(signal)).click()
+        elementFinder('//select[@id="signalSelect"]/option[text()="{}"]'.format(signal))
+        data = []
+        table = driver.find_element_by_xpath('(//div[@id="screener-content"]/descendant::table)[4]')
+        next_page = True
+        while next_page:
+            table = driver.find_element_by_xpath('(//div[@id="screener-content"]/descendant::table)[4]')
+            body = table.find_elements_by_xpath('./descendant::tr')
+            for row in body:
+                cells = row.find_elements_by_xpath('./td')
+                # dont add header row
+                if cells[1].get_attribute('innerText') != 'Ticker':
+                    one_row = []
+                    one_row = [cell.get_attribute('innerText') for cell in cells]
+                    one_row.insert(1,cells[1].find_element_by_xpath('./a').get_attribute('href'))
+                    one_row.extend(['{}'.format(signal)])
+                    data.append(one_row)
+            try:
+                elementFinder('//a[@class="screener_arrow"]','click')
+            except:
+                next_page = False
+        current_signal = pd.DataFrame(data)
+        df = df.append(current_signal)
+        df.to_csv('finviz {}.csv'.format(datetime.today().strftime('%Y-%m-%d')), index=False)
     df.columns = columns
-    df.to_csv('finviz {}.csv'.format(datetime.today().strftime('%Y-%m-%d')),index=False)
-
+    df.to_csv('finviz {}.csv'.format(datetime.today().strftime('%Y-%m-%d')), index=False)
 
 if __name__ == '__main__':
     chromedriver_path = os.path.join(os.path.abspath(os.getcwd()), "chromedriver.exe")
     chrome_options = Options()
-    chrome_options.add_argument("load-extension=C:\\Users\\Shivang Gupta\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\cfhdojbkjhnklbpkdaibdccddilifddb\\3.11_0")
+    chrome_options.add_argument("load-extension=C:\\Users\\Shivang Gupta\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\pkehgijcmpdhfbdbbnkijodmdjhbjlgp\\2021.6.8_0")
     driver = webdriver.Chrome(chromedriver_path,chrome_options=chrome_options)
+    # driver = webdriver.Chrome(chromedriver_path)
     print('Running Chromedriver....')
     # driver.get('https://finviz.com/screener.ashx?v=152&c=0,1,2,3,4,5,6,7,8,9'
     #            ',10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29'
